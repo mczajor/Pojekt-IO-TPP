2b933706776df1a7f7ceac1d71b2c6d4dfe1a659
xramzesx <xramzesbusiness@gmail.com>
2024-03-17 02:36:40 +0100

    [general] add communication examples

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import eel
 
+## INIT EEL ##
 eel.init('web')
 
+## EXAMPLE FROM PYTHON TO JS USAGE ##
+@eel.expose
+def from_python_to_js(num):
+    print("from python",num)
+    return 2 * num
+
+## EXAMPLE FROM JS TO PYTHON USAGE ##
+def handle_js(num):
+    print("from js",num)
+
+eel.from_js_to_python(10)(handle_js)
+
+### EEL SETUP ###
 eel.start("index.html", size = (320, 320))
